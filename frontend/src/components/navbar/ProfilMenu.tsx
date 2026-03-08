ab99aa8d5520738c52d20880dedd833a89c84046
Widoyo <widoyo@gmail.com>
2026-03-08 17:14:40 +0700

    Perubahan Menu Profil ke Mega Dropdown

---
+++ frontend/src/components/navbar/ProfilMenu.tsx
@@ -0,0 +1,199 @@
+import React from "react";
+import { ChevronDown, Boxes, FileText, Info, Phone, MessageCircle } from "lucide-react";
+import { Instagram, Youtube, Facebook, Mail, Map } from "lucide-react";
+import { Link } from "react-router-dom";
+/**
+ * MEGA Dropdown MENU STATIS - 4 KOLOM
+ */
+const ProfilMenu: React.FC<{ label: string }> = ({ label }) => {
+  return (
+    <div className="group flex items-center h-full relative">
+      <button className="font-bold flex items-center text-sm text-indigo p-3 group-hover:bg-slate-50 rounded-t-lg transition-all">
+        {label}
+        <ChevronDown className="ml-1 w-4 h-4 group-hover:rotate-180 transition-transform" />
+      </button>
+
+      {/* Panel Mega Menu */}
+      <div className="absolute top-[80px] right-0 w-[900px] bg-white shadow-2xl rounded-bl-3xl rounded-br-lg border border-gray-100 hidden group-hover:block z-[70] animate-in fade-in zoom-in-95 slide-in-from-right-10 duration-300 overflow-hidden">
+        <div className="grid grid-cols-4 divide-x divide-gray-100">
+          {/* Kolom 1: Data Hidrologi */}
+          <div className="p-5 hover:bg-slate-50 transition-colors">
+            <div className="flex items-center gap-2 mb-6 text-gray-400">
+              <Boxes size={18} />
+              <h4 className="font-bold text-xs uppercase tracking-widest">
+                Struktur &amp; Info
+              </h4>
+            </div>
+            <ul className="space-y-3">
+              <li>
+                <MenuLink
+                  to="/struktur-organisasi"
+                  label="Struktur Organisasi"
+                  desc="Bagan Organisasi Balai"
+                />
+              </li>
+              <li>
+                <MenuLink
+                  to="/tugas-fungsi"
+                  label="Informasi Organisasi"
+                  desc="Tugas Pokok dan Fungsi"
+                />
+              </li>
+              <li>
+                <MenuLink
+                  to="/profil-pejabat"
+                  label="Informasi Pejabat"
+                  desc="Nama dan Foto Pejabat"
+                />
+              </li>
+            </ul>
+          </div>
+
+          {/* Kolom 2: Infrastruktur */}
+          <div className="p-5 hover:bg-slate-50 transition-colors">
+            <div className="flex items-center gap-2 mb-6 text-gray-400">
+              <FileText size={18} />
+              <h4 className="font-bold text-xs uppercase tracking-widest">
+                Satker &amp; Dokumen
+              </h4>
+            </div>
+            <ul className="space-y-3">
+              <li>
+                <MenuLink
+                  to="/satker"
+                  label="Informasi Satker"
+                  desc="Alamat dan Kontak Satker"
+                />
+              </li>
+              <li>
+                <MenuLink
+                  to="/pola-rencana"
+                  label="Pola dan RPSDA"
+                  desc="Dokumen Rencana Strategi Pengelolaan Sumber Daya Air"
+                />
+              </li>
+            </ul>
+          </div>
+
+          {/* Kolom 3: Layanan Publik */}
+          <div className="p-5 hover:bg-slate-50 transition-colors">
+            <div className="flex items-center gap-2 mb-6  text-gray-400">
+              <Info size={18} />
+              <h4 className="font-bold text-xs uppercase tracking-widest">
+                Tentang Kami
+              </h4>
+            </div>
+            <ul className="space-y-3">
+              <li>
+                <MenuLink to="/profil" label="Visi - Misi" desc="Visi - Misi" />
+              </li>
+              <li>
+                <MenuLink to="/sejarah" label="Sejarah" />
+              </li>
+            </ul>
+          </div>
+
+          {/* Kolom 4: Dokumentasi / Portal */}
+          <div className="p-5 bg-indigo text-white">
+            <div className="flex items-center gap-2 mb-4">
+              <Phone size={18} className="text-mango" />
+              <h4 className="font-bold text-xs uppercase tracking-widest text-mango">
+                Kontak &amp; Lokasi
+              </h4>
+            </div>
+            <ul className="text-[12px] leading-relaxed mb-4 opacity-80">
+              <li className="flex items-center gap-2">
+                <Link
+                  to={"https://wa.me/628117887443"} // Ganti dengan nomor resmi BBWS
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-2 hover:text-mango transition-colors group/wa"
+                >
+                  <MessageCircle size={14} className="text-mango" />
+                  <span>Whatsapp</span>
+                </Link>
+              </li>
+              <li className="flex items-center gap-2">
+                <Mail size={14} className="text-mango" />
+                <span>bbws.sumatera8@pu.go.id</span>
+              </li>
+              <li className="flex items-center gap-2 ">
+                <Instagram size={14} className="text-mango" />
+                <span>
+                  <Link
+                    to="https://www.instagram.com/pupr_sda_sumatera8/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Instagram
+                  </Link>
+                </span>
+              </li>
+              <li className="flex items-center gap-2 hover:text-white transition-colors cursor-pointer">
+                <Youtube size={14} className="text-mango" />
+                <span>
+                  <Link
+                    to="https://www.youtube.com/@bbwssumateraviii4910"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Youtube
+                  </Link>
+                </span>
+              </li>
+              <li className="flex items-center gap-2">
+                <Facebook size={14} className="text-mango" />
+                <span>
+                  <Link
+                    to="https://www.facebook.com/bbwss8"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Facebook
+                  </Link>
+                </span>
+              </li>
+              <li className="flex items-center gap-2">
+                <Map size={14} className="text-mango" />
+                <span>
+                  <Link
+                    to="https://maps.app.goo.gl/sNibrVZf7HyLPPGU8"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Google Map
+                  </Link>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+/**
+ * HELPER UNTUK LINK DI DALAM MEGA MENU
+ */
+const MenuLink = ({
+  to,
+  label,
+  desc,
+}: {
+  to: string;
+  label: string;
+  desc?: string;
+}) => (
+  <Link to={to} className="group/link block">
+    <div className="text-sm font-semibold text-gray-800 group-hover/link:text-indigo transition-colors">
+      {label}
+    </div>
+    {desc && (
+      <div className="text-[12px] text-gray-400 leading-tight group-hover/link:text-gray-500">
+        {desc}
+      </div>
+    )}
+  </Link>
+);
+export default ProfilMenu;
--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,8 +1,8 @@
-import { MenuIcon, XIcon } from "lucide-react";
+import { MenuIcon, XIcon, ChevronDown } from "lucide-react";
 import React, { useEffect, useState } from "react";
-
 import Dropdown from "../dropdown";
 import { Link, useLocation } from "react-router-dom";
+import ProfilMenu from "./ProfilMenu";
 
 interface NavbarProps {
   backgroundColor?: string;
@@ -13,7 +13,7 @@ interface NavbarProps {
   links?: {
     path: string;
     label: string;
-    submenu: { path: string; label: string }[];
+    submenu?: { path: string; label: string }[];
   }[];
   linksPosition?: "left" | "center" | "right";
   position?: "fixed" | "sticky";
@@ -35,123 +35,114 @@ const Navbar: React.FC<NavbarProps> = ({
   const [activeSubmenu, setActiveSubmenu] = useState<number | null>(null);
   const location = useLocation();
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
+  const toggleMenu = () => setMenuOpen(!menuOpen);
   const toggleSubmenu = (index: number) => {
     setActiveSubmenu((prev) => (prev === index ? null : index));
   };
 
+  useEffect(() => {
+    setMenuOpen(false);
+    setActiveSubmenu(null);
+  }, [location.pathname]);
+
   const getLinksPositionClasses = () => {
     switch (linksPosition) {
-      case "center":
-        return "justify-center";
-      case "right":
-        return "justify-end";
-      default:
-        return "justify-start";
+      case "center": return "justify-center";
+      case "right": return "justify-end";
+      default: return "justify-start";
     }
   };
 
-  useEffect(() => {
-    // Menutup menu saat path berubah
-    setMenuOpen(false);
-    setActiveSubmenu(null); // Menutup submenu
-  }, [location.pathname]); // Memonitor perubahan path
-
-  const positionClass =
-    position === "fixed"
-      ? "fixed top-0 left-0 w-full z-50"
-      : "sticky top-0 z-50";
+  const positionClass = position === "fixed" ? "fixed top-0 left-0 w-full z-50" : "sticky top-0 z-50";
 
   return (
-    <nav
-      className={`${transparent ? "bg-transparent" : "bg-white"} ${shadow} ${
-        position ? positionClass : ""
-      } transition-colors duration-300 border-b-8 border-b-mango`}
-    >
+    <nav className={`${transparent ? "bg-transparent" : "bg-white"} ${shadow} ${positionClass} transition-colors duration-300 border-b-8 border-b-mango`}>
       <div className={`w-full ${maxWidth} mx-auto px-4`}>
         <div className="flex justify-between h-20 items-center">
+          {/* Logo Section */}
           {logoIcon && (
             <Link to={"/"} className="flex items-center gap-2">
               {logoIcon}
               <div className="flex flex-col uppercase text-indigo">
-                <span className="text-xs font-semibold">
-                  Kementerian Pekerjaan Umum
-                </span>
-                <span className="text-xs font-semibold">
-                  Direktorat Jenderal Sumber Daya Air
-                </span>
+                <span className="text-xs font-semibold">Kementerian Pekerjaan Umum</span>
+                <span className="text-xs font-semibold">Direktorat Jenderal Sumber Daya Air</span>
                 <span className="text-sm">BBWS Sumatera VIII</span>
               </div>
             </Link>
           )}
+          
           <span className="uppercase text-indigo">{logoText}</span>
+
+          {/* Mobile Toggle */}
           <div className="flex items-center min-[1021px]:hidden">
-            <button
-              onClick={toggleMenu}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-            >
+            <button onClick={toggleMenu} className="p-2 rounded-md text-gray-700 focus:outline-none">
               {menuOpen ? <XIcon /> : <MenuIcon />}
             </button>
           </div>
+
+          {/* Desktop Navigation */}
           {links && (
-            <nav
-              className={`hidden min-[1021px]:flex h-full gap-5 flex-1 ${getLinksPositionClasses()} ml-4`}
-            >
-              {links.map((link, index) =>
-                link.submenu ? (
-                  <Dropdown
-                    key={index}
-                    label={link.label}
-                    submenu={link.submenu}
-                  />
+            <nav className={`hidden min-[1021px]:flex h-full gap-5 flex-1 ${getLinksPositionClasses()} ml-4`}>
+              {links.map((link, index) => {
+                // KONTEN KHUSUS INDEX 0 (Misal: Beranda atau Highlight)
+                if (index === 0) {
+                  return (
+                    <Link key={index} to={link.path} className="font-medium flex items-center text-base text-black p-2 hover:text-indigo">
+                      {link.label}
+                    </Link>
+                  );
+                }
+
+                // KONTEN KHUSUS INDEX 1: MEGA MENU
+                if (index === 1) {
+                  return (
+                    <ProfilMenu key={index} label={link.label} />
+                  );
+                }
+
+                // LOGIKA BAWAAN (Index 2 ke atas)
+                return link.submenu ? (
+                  <Dropdown key={index} label={link.label} submenu={link.submenu} />
                 ) : (
                   <Link
                     key={index}
                     to={link.path}
-                    className={`font-medium flex items-center text-base transition-colors hover:underline text-black p-2`}
-                    onClick={() =>
-                      link.submenu ? toggleSubmenu(index) : setMenuOpen(false)
-                    }
+                    className="font-medium flex items-center text-base transition-colors hover:underline text-black p-2"
+                    onClick={() => link.submenu ? toggleSubmenu(index) : setMenuOpen(false)}
                   >
                     {link.label}
                   </Link>
-                )
-              )}
+                );
+              })}
             </nav>
           )}
         </div>
       </div>
+
+      {/* Mobile Menu */}
       {links && menuOpen && (
-        <div
-          className={`min-[1021px]:hidden px-2 pb-3 fixed ${backgroundColor} ${shadow} w-full`}
-        >
+        <div className={`min-[1021px]:hidden px-2 pb-3 fixed ${backgroundColor} ${shadow} w-full max-h-[calc(100vh-80px)] overflow-y-auto`}>
           {links.map((link, index) => (
-            <div key={index}>
-              <Link
-                to={link.path}
-                className={`block px-3 py-2 rounded-md text-base font-medium ${
-                  activeSubmenu === index ? "text-blue-500" : "text-gray-700"
-                } hover:bg-gray-100 hover:underline`}
-                onClick={() =>
-                  link.submenu ? toggleSubmenu(index) : setMenuOpen(false)
-                }
+            <div key={index} className="border-b border-gray-100 last:border-none">
+              <div 
+                className={`flex justify-between items-center px-3 py-3 rounded-md text-base font-medium ${activeSubmenu === index ? "text-indigo" : "text-gray-700"}`}
+                onClick={() => link.submenu ? toggleSubmenu(index) : setMenuOpen(false)}
               >
-                {link.label}
-              </Link>
+                {link.submenu ? (
+                  <span>{link.label}</span>
+                ) : (
+                  <Link to={link.path} className="w-full">{link.label}</Link>
+                )}
+                {link.submenu && <ChevronDown className={`w-4 h-4 transition-transform ${activeSubmenu === index ? "rotate-180" : ""}`} />}
+              </div>
+              
               {link.submenu && activeSubmenu === index && (
-                <div className="ml-4">
+                <div className="ml-4 bg-gray-50 rounded-lg mb-2">
                   {link.submenu.map((submenuItem, subIndex) => (
                     <Link
                       key={subIndex}
                       to={submenuItem.path}
-                      className={`block px-3 py-2 rounded-md text-sm font-medium hover:text-gray-900 hover:bg-gray-50 ${
-                        location.pathname === submenuItem.path
-                          ? "font-bold text-blue-500"
-                          : "text-gray-600"
-                      }`}
+                      className={`block px-4 py-2 text-sm font-medium hover:text-indigo ${location.pathname === submenuItem.path ? "text-indigo font-bold" : "text-gray-600"}`}
                     >
                       {submenuItem.label}
                     </Link>
